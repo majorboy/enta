77ed48b7bd3497fd0c7c1165fe0397bfc9f04453
KengoNagata <majorboy47@gmail.com>
2019-08-30 06:01:13 +0900

    postsテーブルを作成

--- db/migrate/20190829205602_create_posts.rb
+++ db/migrate/20190829205602_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :name
+      t.text  :have_done
+      t.text :to_do
+      t.text :comment
       t.timestamps
     end
   end
