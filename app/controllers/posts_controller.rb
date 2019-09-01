96b5da69af38113a09bc4e15a57c07b45472c960
KengoNagata <majorboy47@gmail.com>
2019-09-01 16:40:58 +0900

    ページネーションを実装した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order('created_at DESC')
+    @posts = Post.order('created_at DESC').page(params[:page]).per(9)
   end
 
   def new
