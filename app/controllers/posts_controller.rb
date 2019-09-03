a17328ecc376f6edd8c77bebd2f6d22ce4849b45
KengoNagata <majorboy47@gmail.com>
2019-09-04 06:14:40 +0900

    投稿機能　user_idも保存できるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    Post.create(name: post_params[:name], have_done:post_params[:have_done],to_do: post_params[:to_do], comment: post_params[:comment], user_id: current_user.id )
   end
 
   def update
