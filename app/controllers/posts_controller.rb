c1761815c2acd3d3a85daaceda15aa14077a5e56
KengoNagata <majorboy47@gmail.com>
2019-09-05 06:07:20 +0900

    コメント機能修正 -ログイン時のみコメント投稿画面に遷移出来るようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :move_to_index , except: :index
+  
   def index
     @posts = Post.includes(:user).order('created_at DESC').page(params[:page]).per(9)
   end
