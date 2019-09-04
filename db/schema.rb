bf4e5e1ba72b49bb0cfdd62c5308c317bf9f6d22
KengoNagata <majorboy47@gmail.com>
2019-09-04 19:12:49 +0900

    postsテーブル　不要になったnameカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_121603) do
+ActiveRecord::Schema.define(version: 2019_09_04_101148) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "have_done"
     t.text "to_do"
     t.text "comment"
